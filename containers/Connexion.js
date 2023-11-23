0991ac7a7c2c5c6cf24dd125e6725879f5a19b4d
joanzafdev <joanzafdev@gmail.com>
2023-11-23 11:07:42 +0100

    initiate stylesheet on Connexion screen

--- containers/Connexion.js
+++ containers/Connexion.js
@@ -26,33 +26,62 @@ function Connexion ({navigation}){
             .catch(error => alert(JSON.stringify(error)));
     }
     return (
-      <View style={{display:"flex", flexDirection:"column", alignItems :"center", width :"50%", backgroundColor :"white"}}>
-        <Text>Connexion</Text>
-        <View>
+      <View style={styles.mainCard}>
+        <View style={{...styles.cardCentered, ...styles.shadowProp}}>
+            <Text>Connexion</Text>
             <View>
-                <Text>Identifiant ou email</Text>
-                <TextInput 
-                    defaultValue={email}
-                    placeholder= "Email..."
-                    onChangeText={ newEmail => setMail(newEmail)}
-                />
+                <View>
+                    <Text>Identifiant ou email</Text>
+                    <TextInput 
+                        defaultValue={email}
+                        placeholder= "Email..."
+                        onChangeText={ newEmail => setMail(newEmail)}
+                    />
+                </View>
+                <View>
+                    <Text>Mot de passe</Text>
+                    <TextInput 
+                        secureTextEntry={true}
+                        onChangeText={ pass => setPassword(pass)}
+                    />
+                </View>
+                <TouchableOpacity onPress={onPress}>
+                    <Text>Se connecter</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate("ForgotPassword") }>
+                    <Text style={{fontSize :8, color : "grey"}} >Mot de passe oublié</Text>
+                </TouchableOpacity>
             </View>
-            <View>
-                <Text>Mot de passe</Text>
-                <TextInput 
-                    secureTextEntry={true}
-                    onChangeText={ pass => setPassword(pass)}
-                />
-            </View>
-            <TouchableOpacity onPress={onPress}>
-                <Text>Se connecter</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("ForgotPassword") }>
-                <Text style={{fontSize :8, color : "grey"}} >Mot de passe oublié</Text>
-            </TouchableOpacity>
         </View>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+    mainCard : {
+        display:"flex",
+        flexDirection:"column",
+        alignItems :"center"
+    },
+    cardCentered : {
+        width : "50%",
+        display :"flex",
+        flexDirection:"column",
+        alignItems :"center" ,
+        marginTop :30,
+        padding :20,
+        backgroundColor :"white"
+    },
+    shadowProp: {
+        shadowColor: '#171717',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+});
+
+
+
+
 
 export default Connexion
